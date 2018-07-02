103de7a63c0e3a738a42289790fb2976042d0b94
Serdar Sayin <srdrsayin@gmail.com>
2018-07-02 15:30:03 +0300

    Install Bulma and Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
+import { UpdateComponent } from './update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ListComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
